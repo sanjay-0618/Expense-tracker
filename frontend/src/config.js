a3d0234cd6cc29b8c01b1f57b34ceb6c7743bebd
sanjay-0618 <sanjaytnj21@gmail.com>
2025-08-31 19:11:29 +0530

    fix: update API URL in frontend config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 
-const BACKEND_URL = "https://fqrvf7zq-5000.inc1.devtunnels.ms/";
+const BACKEND_URL = "https://expense-tracker-sanjay-ace.onrender.com/";
 
 export default BACKEND_URL;
